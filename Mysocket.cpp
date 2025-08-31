e68f67b8f6b99ef5e3ccdcc078b8a8e42a48ea15
LonelyPower <1551741260@qq.com>
2025-08-31 15:55:44 +0800

    封装socket

---
+++ Mysocket.cpp
@@ -0,0 +1,48 @@
+#include "Mysocket.h"
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+
+Mysocket::Mysocket() {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&addr, 0, sizeof(addr));
+    
+    if (sockfd < 0) {
+        perror("socket create failed");
+    }
+}
+
+Mysocket::~Mysocket() {
+    if (sockfd >= 0) {
+        close(sockfd);
+    }
+}
+
+void Mysocket::bindAddr(const std::string& ip, int port);
+{
+    
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    addr.sin_port = htons(port);
+
+    if (bind(sockfd, (sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind failed");
+    }
+}
+
+void Mysocket::startListen(int backlog) {
+    if (listen(sockfd, backlog) < 0) {
+        perror("listen failed");
+    }
+}
+
+int Mysocket::acceptConn() {
+    sockaddr_in c_addr{};
+    socklen_t c_addr_len = sizeof(c_addr);
+    int cfd = accept(sockfd, (sockaddr *)&c_addr, &c_addr_len);
+    if (cfd < 0) {
+        perror("accept failed");
+    }
+    return cfd;
+}
---
+++ Mysocket.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <netinet/in.h> // sockaddr_in
+
+class Mysocket {
+public:
+    Mysocket();                          // 构造函数：创建 socket
+    ~Mysocket();                         // 析构函数：关闭 socket
+
+    void bindAddr(const std::string& ip, int port); // 绑定 IP 和端口
+    void startListen(int backlog = 5);   // 开始监听
+    int  acceptConn();                   // 接受连接，返回客户端 fd
+
+    int  getFd() const { return sockfd; } // 获取 socket fd
+
+private:
+    int sockfd;
+    sockaddr_in addr;
+};
